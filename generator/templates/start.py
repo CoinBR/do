c852a5a4b4a4f2d206529538c2d2fb0572372c51
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-18 13:19:15 -0300

    Deploy start script for test

--- generator/templates/start.py
+++ generator/templates/start.py
@@ -6,20 +6,22 @@ from collections import deque as dq
 BASE_CMD = 'docker-compose down && docker-compose up'
 FLAGS = ' ' + ' '.join(sys.argv) if len(sys.argv) else False
 
-def start(path: str=DIRS['project'], serve: bool=False):
-    cmd = BASE_CMD + FLAGS 
-    os.system('cd {} && {}'.format(path, cmd))
-    try_start_submodules(path)
 
-def try_start_submodules(path: str):
-    subroot = path + '/submodules'
+def start():
+    cmd = BASE_CMD + FLAGS 
+    os.system('cd {} && {}'.format(DIRS['project'], cmd))
+
+def try_start_submodules():
+    subroot = os.path.join(DIRS['project'], 'submodules')
     if not (os.path.exists(subroot) and os.path.isdir(subroot)):
         return
 
     projs = [d for d in os.listdir(subroot) 
             if os.path.isdir(os.path.join(subroot, d))]
-    dq(os.system(os.path.join(subroot, proj, 'do')) for proj in projs)
+
+    dq(os.system(os.path.join(subroot, proj, 'do', 'start') + FLAGS) for proj in projs)
     
-if CFG['service'] == '':
-    print()
-    exit()
+if CFG['service'] != '':
+    start()
+
+try_start_submodules()
