3376223f3e4016f7cbdaf19c64cb3716fa5dec7c
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-19 10:25:53 -0300

    Move launch and run of scripts to prefix.py

--- generator/templates/start.py
+++ generator/templates/start.py
@@ -1,36 +1,6 @@
-import os
-import sys
-from collections import deque as dq
-
-
 BASE_CMD = 'docker-compose down && docker-compose up'
-FLAGS = ' ' + ' '.join(sys.argv) if len(sys.argv) else False
 
-
-def start():
-    cmd = BASE_CMD + FLAGS 
-    os.system('cd {} && {}'.format(DIRS['project'], cmd))
-
-def try_start_submodules():
-    
-    def launch(proj_dir):
-        # generate scripts, b4 starting
-        paths['gen'] = os.path.join(proj_dir, 'do', 'generator', 'generate.py') 
-        paths['start'] = os.path.join(proj_dir, 'do', 'start') 
-        dq(os.system(path) for path in paths) 
-
-    subroot = os.path.join(DIRS['project'], 'submodules')
-    if not (os.path.exists(subroot) and os.path.isdir(subroot)):
-        return
-
-    projs = [d for d in os.listdir(subroot) 
-            if os.path.isdir(os.path.join(subroot, d))]
-
-    dq(os.system(os.path.join(subroot, proj, 'do', 'start') + FLAGS) for proj in projs)
-    dq(os.system(os.path.join(subroot, proj, 'do', 'start') + FLAGS) for proj in projs)
-
-    
 if CFG['service'] != '':
-    start()
+    run(BASE_CMD, argv)
 
-try_start_submodules()
+try_launch_in_submodules('start')
