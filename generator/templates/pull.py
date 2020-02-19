37fff9fcf667135f0ff000a8d96f56c5144d0057
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-19 12:31:00 -0300

    Update Pull

--- generator/templates/pull.py
+++ generator/templates/pull.py
@@ -1,2 +1,12 @@
-run('git pull --recurse-submodules --jobs 4 && git submodule update --init --recursive --jobs 4')
 try_launch_in_submodules('pull')
+
+run([
+    'git pull --recurse-submodules --jobs 4',
+    'git submodule update --init --recursive --jobs 4',
+    'git add .gitmodules',
+    'git add submodules',
+    'git commit -m "Updating submodules" && git push origin HEAD',
+ ]
+
+
+
