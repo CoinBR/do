a5a1fc5bf4be8970ee9aba38b28476e23cdf1e84
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-19 13:02:13 -0300

    Try fix pull

--- generator/templates/pull.py
+++ generator/templates/pull.py
@@ -1,10 +1,14 @@
+PULLCMD = 'git pull --recurse-submodules --jobs 4'
+
+run(PULLCMD)
+run('git submodule update --init --recursive --jobs 4')
+run(PULLCMD, cd=dirs['do'])
+
 try_launch_in_submodules('pull')
 
 run([
-    'git pull --recurse-submodules --jobs 4',
-    'git submodule update --init --recursive --jobs 4',
     'git add .gitmodules',
     'git add submodules',
-    'git commit -m "Updating submodules" && git push origin HEAD',
+    'git add do'
+    'git commit -m "Update submodules" && git push origin HEAD',
  ])
-
