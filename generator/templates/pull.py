30a5514919382f65176a13d36dc5479e242b9dff
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-19 13:48:05 -0300

    Explicit specify 'do' directory

--- generator/templates/pull.py
+++ generator/templates/pull.py
@@ -4,7 +4,7 @@ CHECKOUT = 'git checkout master'
 run('git submodule update --init --recursive --jobs 4')
 run([CHECKOUT, PULL, 'generator/generate.py'], cd=DIRS['do'])
 
-try_launch_in_submodules([CHECKOUT, 'pull'])
+try_launch_in_submodules([CHECKOUT, 'do/pull'])
 
 run([
     'git add .gitmodules',
