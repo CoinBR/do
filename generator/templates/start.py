3ddd8c1912f93df51e448d531a5c34667146e665
Pedro "Coin" Duarte <coinbr@gmail.com>
2020-02-27 11:11:39 -0300

    Create function to run cmds recursively in projs

--- generator/templates/start.py
+++ generator/templates/start.py
@@ -1,6 +1,1 @@
-BASE_CMD = 'docker-compose down && docker-compose up'
-
-if CFG['service'] != '':
-    run(BASE_CMD, argv)
-
-try_launch_in_submodules('start')
+try_run_rec('docker-compose down && docker-compose up')
